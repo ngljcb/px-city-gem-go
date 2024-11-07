8f588bfbf641daef7398598fd556e66092dd8c8c
Jake / <75119871+ngljcb@users.noreply.github.com>
2024-11-07 21:17:49 +0100

    feat: time formatter component

--- app/results.tsx
+++ app/results.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import { styles } from '../constants/styles/Default';
+import { formatTime } from '@/utils/timeFormatter';
 
 export default function Results() {
   const router = useRouter();
-  const { totalTime } = useLocalSearchParams(); // Passa il tempo totale come parametro
-
-  // Funzione per formattare il tempo in minuti e secondi
-  const formatTime = (milliseconds: number) => {
-    const totalSeconds = Math.floor(milliseconds / 1000);
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  };
+  const { totalTime } = useLocalSearchParams(); // Passa il tempo totale come param
 
   return (
     <ImageBackground source={require('@/assets/images/results-backdrop.gif')} style={styles.container} resizeMode="cover">
