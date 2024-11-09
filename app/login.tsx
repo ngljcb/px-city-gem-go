a36df1cb1feb7585de802edfd404b0223f421c5f
Jake / <75119871+ngljcb@users.noreply.github.com>
2024-11-09 11:34:29 +0100

    feat: login

--- app/login.tsx
+++ app/login.tsx
@@ -5,11 +5,13 @@ import { styles } from '../styles/Login';
 import { FIREBASE_AUTH } from '../FirebaseConfig';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { router } from 'expo-router';
+import FirebaseManager from '../services/FirebaseManager';
 
 const Login = () => {
   const { type } = useLocalSearchParams<{ type: string }>();
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const auth = FIREBASE_AUTH;
 
@@ -28,11 +30,23 @@ const Login = () => {
   const signUp = async () => {
     setLoading(true);
     try {
-      const user = await createUserWithEmailAndPassword(auth, email, password);
-      if (user) router.replace('/routes');
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      if (user) {
+        // Salva username e uid dell'utente nella collezione /Users
+        const firebaseManager = new FirebaseManager();
+        await firebaseManager.addDocument('Users', {
+          uid: user.uid,
+          username: username,
+        });
+
+        // Naviga alla pagina routes
+        router.replace('/routes');
+      }
     } catch (error: any) {
       console.log(error);
-      alert('Sign in failed: ' + error.message);
+      alert('Sign up failed: ' + error.message);
     }
     setLoading(false);
   };
@@ -49,6 +63,7 @@ const Login = () => {
 
       <View style={{ marginBottom: 20 }}>
         <TextInput autoCapitalize="none" placeholder="Email" style={styles.inputField} value={email} onChangeText={setEmail} />
+        <TextInput autoCapitalize="none" placeholder="Username" style={styles.inputField} value={username} onChangeText={setUsername} />
         <TextInput
           autoCapitalize="none"
           placeholder="Password"
@@ -65,7 +80,7 @@ const Login = () => {
         </TouchableOpacity>
       ) : (
         <TouchableOpacity onPress={signUp} style={[styles.btn, styles.btnPrimary]}>
-          <Text style={styles.btnPrimaryText}>Create acount</Text>
+          <Text style={styles.btnPrimaryText}>Create account</Text>
         </TouchableOpacity>
       )}
     </KeyboardAvoidingView>
